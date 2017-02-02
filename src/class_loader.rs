8b55e685433abfda72a151d508e261e10fe73a57
Mackie Loeffel <mackie.loeffel@web.de>
2017-02-02 22:43:24 +0100

    added classloader and .travis.yml

---
+++ src/class_loader.rs
@@ -0,0 +1,24 @@
+use classfile_parser::{ClassFile, parse_class};
+
+// see https://docs.oracle.com/javase/specs/jvms/se6/html/ConstantPool.doc.html
+
+pub struct ClassLoader {
+    // TODO: convert to Path when classfile_parser is changed
+    load_dir: &str,
+}
+
+impl ClassLoader {
+    pub fn new(load_dir: &str) -> ClassLoader {
+        ClassLoader { load_dir: load_dir }
+    }
+
+    // pub fn load_class(name: &str) -> Result<>
+}
+
+pub enum ClassLoadingError {
+    NoClassDefFound,
+    ClassFormatError,
+    UnsupportedClassVersion,
+    IncompatibleClassChange,
+    ClassCircularity,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 extern crate classfile_parser;
 
+mod class_loader;
+
+use class_loader::ClassLoader;
+
 fn main() {
+    let classloader = ClassLoader::new("./assets");
+
     let parsed_class = classfile_parser::parse_class("./assets/SimpleClass").unwrap();
     println!("Version: {}.{}", parsed_class.minor_version, parsed_class.major_version);
 }
